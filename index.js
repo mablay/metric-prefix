c39de2cfaca1eeb7897d0637a5f3b4345996bc85
Marc Ablay <marc.ablay@gmail.com>
2018-09-25 23:36:14 +0200

    cleaned code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Big = require('big.js')
 
-const prefixes = ['', ...'kMGTPEZYXWV'.split('')]
+const ZIDX = 11
+const prefixes = 'vwxyzafpnµmkMGTPEZYXWV'.split('')
+prefixes.splice(ZIDX, 0, '')
 
 /**
  * Format a number using an SI prefix and only significant digits.
@@ -10,11 +12,15 @@ const prefixes = ['', ...'kMGTPEZYXWV'.split('')]
  * @param  {String} [delimiter=' '] between number and prefix
  * @return {String}                 formatted number
  */
-function metric (number, { unit = '', precision = 3, delimiter = ' ' } = {}) {
+function metric (number, {
+  unit = '',
+  precision = 3,
+  delimiter = ' '
+} = {}) {
   const N = (number instanceof Big) ? number : new Big(number)
-  const prefix = prefixes[Math.floor(N.e / 3)]
-  N.e = N.e % 3
+  const prefix = prefixes[ZIDX + Math.floor(N.e / 3)]
   N.c = N.c.slice(0, precision)
+  N.e = (N.e >= 0) ? N.e % 3 : 2 + ((N.e + 1) % 3)
   const dl = (prefix === '' && unit === '') ? '' : delimiter
   return `${N.toPrecision(precision)}${dl}${prefix}${unit}`
 }
--- test/spec.js
+++ test/spec.js
@@ -2,7 +2,7 @@ const test = require('tape')
 const metric = require('../')
 const unit = 'Hz' // some example unit
 
-test('floor', t => {
+test('big', t => {
   t.equal(metric(7), '7.00', 'no conversion')
   t.equal(metric(12, {unit}), '12.0 Hz', 'custom unit')
   t.equal(metric(999), '999', 'no conversion')
@@ -17,5 +17,18 @@ test('floor', t => {
   t.equal(metric(10.9e9), '10.9 G', 'giga converstion')
   t.equal(metric(100e15), '100 P', 'peta converstion')
   t.equal(metric('100000000000000000000000000'), '100 Y', 'yotta conversion')
+  t.equal(metric(1e35), '100 V', 'veca conversion')
+  t.end()
+})
+
+test('small', t => {
+  t.equal(metric(0.7), '700 m', 'milli conversion')
+  t.equal(metric(0.01), '10.0 m', 'milli conversion')
+  t.equal(metric(0.001), '1.00 m', 'milli conversion')
+  t.equal(metric(0.001239), '1.23 m', 'milli conversion')
+  t.equal(metric(0.0001), '100 µ', 'mycro conversion')
+  t.equal(metric(0.00099999), '999 µ', 'mycro conversion')
+  t.equal(metric('0.000000000000000000000000000000001'), '1.00 v', 'vocta conversion')
+  t.equal(metric(0.000000000000000000000000000000001), '1.00 v', 'vocta conversion')
   t.end()
 })
